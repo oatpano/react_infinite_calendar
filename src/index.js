8270f24d81923be3845ef7ffebd237aa539c4a66
oatrice msi <anirut.311@gmail.com>
2017-05-17 22:39:23 +0700

    add basic code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import InfiniteCalendar from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css'; // Make sure to import the default stylesheet
+
+// Render the Calendar
+var today = new Date();
+var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
 
 ReactDOM.render(
-  <App />,
+  <InfiniteCalendar
+    width={400}
+    height={600}
+    selected={today}
+    disabledDays={[0,6]}
+    minDate={lastWeek}
+  />,
   document.getElementById('root')
-);
+)
